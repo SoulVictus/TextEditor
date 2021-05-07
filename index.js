626eb7fe041e52ed72e14d87540970b09395b609
Slvi <jakub.lorek44@gmail.com>
2021-05-07 10:36:36 +0200

    final improvements

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let textArea = document.getElementById("text-field")
-let buttons = document.getElementsByTagName("button")
+let textArea = document.getElementById("text-field");
+let buttons = document.getElementsByTagName("button");
 const inputElement = document.getElementById("file-selector");
 inputElement.addEventListener("change", importJSON, false);
 
@@ -10,7 +10,7 @@ textArea.focus();
 for (let btn of buttons) {
     btn.addEventListener('click', () => {
         if (btn.className === "btn-command") {
-            let command = btn.dataset['command']
+            let command = btn.dataset['command'];
             document.execCommand(command, false, null);
             textArea.focus();
         }
@@ -23,32 +23,32 @@ const exportToJSON = () => {
     /* Create json file*/
     let textContent = JSON.stringify({
         content: textArea.innerHTML
-    })
+    });
 
     let file = new File([textContent], "newfile.json", {
         type: "text/json",
     });
 
     /* Open download link*/
-    let a = document.createElement("a")
-    a.setAttribute("href", URL.createObjectURL(file))
+    let a = document.createElement("a");
+    a.setAttribute("href", URL.createObjectURL(file));
     a.setAttribute("download", "newfile.json");
-    document.body.appendChild(a)
+    document.body.appendChild(a);
     a.click();
-    document.body.removeChild(a)
+    document.body.removeChild(a);
 }
 
 function importJSON() {
     
     /* Import json and open file */
-    const file = this.files[0]
+    const file = this.files[0];
     fileReader = new FileReader();
     fileReader.onload = receivedText;
     fileReader.readAsText(file);
 
     function receivedText(e) {
         let lines = e.target.result;
-        let fileContent = JSON.parse(lines)
+        let fileContent = JSON.parse(lines);
         textArea.innerHTML = fileContent.content;
     }
 }
